44d3caf80e9294b4094d30b98f11aa0bcc812799
LyadochkinaPolina <lpolina2810@gmail.com>
2025-09-12 20:06:47 +0300

    Add files via upload

--- Lab1/Blue.cs
+++ Lab1/Blue.cs
@@ -7,7 +7,18 @@
             bool answer = false;
 
             // code here
-
+            if (a==0 || b == 0)
+            {
+                answer = false;
+            }
+            if (a > 0 && b > 0)
+            {
+                answer = true;
+            }
+            else if (a < 0 && b < 0)
+            { 
+                answer = true;
+            }
             // end
 
             return answer;
@@ -17,7 +28,10 @@
             bool answer = false;
 
             // code here
-
+            if (d != Math.Round(d))
+            {
+                answer = true;
+            }
             // end
 
             return answer;
@@ -27,40 +41,76 @@
             bool answer = false;
 
             // code here
-
+            if (a % b == 0)
+            {
+                answer = true;
+            }
+            else {answer = false; }
+            if (b == 0)
+            {
+                answer = false;
+            }
             // end
 
-            return answer;
+                return answer;
         }
         public double Task4(double d, double f, double g)
         {
             double answer = 0;
 
             // code here
+            if((Math.Abs(d) > Math.Abs(f)) && (Math.Abs(d) > Math.Abs(g)))
+            {
+                answer = d;
+            }
+            if ((Math.Abs(f) > Math.Abs(d)) && (Math.Abs(f) > Math.Abs(g)))
+            {
+                answer = f;
+            }
+            if ((Math.Abs(g) > Math.Abs(d)) && (Math.Abs(g) > Math.Abs(f)))
+            {
+                answer = g;
+            }
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
         public double Task5(double x)
         {
             double answer = 0;
 
             // code here
+            if (x <= -1)
+            {
+                answer = 0;
+            }
+            else if (-1<x && x <= 0)
+            {
+                answer = x + 1;
+            }
+            else if (x > 0)
+            {
+                answer = 1;
+            }
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
         public bool Task6(double circleS, double squareS)
         {
             bool answer = false;
 
             // code here
+            double storona = Math.Sqrt(squareS);
+            double diametr = 2 * (Math.Sqrt(circleS / Math.PI));
+            if (storona - diametr >= 0)
+            {
+                answer = true;
+            }
+            else { answer = false; }
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
 
         public double Task7(double d, double f)
--- Lab1/Green.cs
+++ Lab1/Green.cs
@@ -7,7 +7,7 @@
             bool answer = false;
 
             // code here
-
+            answer = Math.Abs(d) >= 1;
             // end
 
             return answer;
@@ -17,17 +17,18 @@
             bool answer = false;
 
             // code here
-
+            answer = ((d + f) / 2) > 0;
             // end
 
             return answer;
+
         }
         public bool Task3(int a, int b)
         {
             bool answer = false;
 
             // code here
-
+            answer = (a + b) > Convert.ToDouble(((Math.Abs(a) + Math.Abs(b) / 2)));
             // end
 
             return answer;
@@ -37,7 +38,7 @@
             int answer = 0;
 
             // code here
-
+            answer = Math.Max(a, Math.Max(b, c));
             // end
 
             return answer;
@@ -47,7 +48,14 @@
             double answer = 0;
 
             // code here
-
+            if (Math.Abs(x) > 1)
+            {
+                answer = 0;
+            }
+            else if (Math.Abs(x) <= 1)
+            {
+                answer = Math.Pow(x, 2) - 1;
+            }
             // end
 
             return answer;
@@ -57,7 +65,22 @@
             bool answer = false;
 
             // code here
-
+            if (x < 0)
+            {
+                answer =  y <= (1 + x);
+            }
+            else
+            {
+                answer = y <= (1 - x);
+            }
+            if (x < -1 || x > 1)
+            {
+                answer = false;
+            }
+            if (y < 0)
+            {
+                answer = false;
+            }
             // end
 
             return answer;
@@ -68,20 +91,53 @@
             bool answer = true;
 
             // code here
-
+            if (n < 0)
+            {
+                answer = false;
+            }
+            else if (n%2 == 0)
+            {
+                answer = false;
+            }
+            else { answer = true; }
             // end
 
-            return answer;
+                return answer;
         }
         public bool Task8(int X, int Y)
         {
             bool answer = false;
 
             // code here
+            int gotosleep = 4*60;
+            int wakeup = 14*60;
+            for (int i = 1; i <= X; i++)
+            {
+                if (i%2 == 1)
+                {
+                    gotosleep = (gotosleep - Y) % 1440; //1440 - минут в сутках
+                    while (gotosleep < 0)
+                    {
+                        gotosleep += 1440;
+                       
+                    }
 
-            // end
+                }
+                if (wakeup > 7*60)
+                {
+                    wakeup -= 60;
+                }
+                int duration = (wakeup - gotosleep + 1440) % 1440;
+                if (wakeup == 7 * 60 && duration >= 7 * 60 && duration <= 9 * 60)
+                {
+                    answer = true;
+                }
+                else { answer = false; }
+            }
 
-            return answer;
+                // end
+
+                return answer;
         }
     }
 }
